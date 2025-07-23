2e066dcbe7172cb8726934b7bef69dc962909ed8
Rewan Elsayed <rewan.elsayed2003@gmail.com>
2025-07-23 22:34:37 +0300

    Add files via upload

---
+++ Models/Dtos/CreateProductItemDto.cs
@@ -0,0 +1,7 @@
+﻿public class CreateProductItemDto
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public bool OutOfStock { get; set; }
+    public string Type { get; set; }
+}
